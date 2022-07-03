196ff06d314a1944cb299e377b369d4bad8625ed
MoatazAhmed26 <56225599+MoatazAhmed26@users.noreply.github.com>
2022-07-03 05:56:37 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.target', {
+    strings: ["Easy journey.......", "Fast speed......"],
+    typeSpeed: 60
+});
+jQuery(document).ready(function() {
+
+    jQuery('#load').fadeOut(4000);
+
+});
